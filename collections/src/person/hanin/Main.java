49cee382f3329818823fe5c3cef7d671ebd70c57
S-Hanin <sign.rx@yandex.ru>
2020-03-06 15:30:32 +0300

    add task3

--- collections/src/person/hanin/Main.java
+++ collections/src/person/hanin/Main.java
@@ -1,18 +1,69 @@
 package person.hanin;
 
-import java.util.*;
-import java.util.function.Consumer;
-import java.util.stream.Collectors;
 import io.humb1t.Main.Order;
 import io.humb1t.Main.OrderStatus;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 
 public class Main {
     public static void main(String[] args) {
+//        task3();
         task5();
     }
 
-    private void task2(){
+    private static void task1() {
+        Collection<String> c = Collections.EMPTY_LIST;
+        List<String> list = new ArrayList<>(c);
+
+        List<Order> orders = Collections.singletonList(new Order(OrderStatus.COMPLETED, 50));
+        /*
+         * Наиболее очевидное решение, просто читать,
+         * легко понять намерения автора.
+         * Все паттерны перебора коллекции скрыты "под капотом",
+         * остаются только операции, которые нужно провести над коллекцией
+         */
+        orders.stream()
+                .filter(order -> order.status == OrderStatus.COMPLETED)
+                .forEach(order -> System.out.println(order.toString()));
+
+        /*
+         * Тоже простое решение для перебора, паттерн "итератор" скрыт
+         * конструкцией цикла.
+         * Однако, если бы нужно было отфильтровать коллекцию, пришлось бы
+         * создать новую коллекцию и использовать условия if или switch.
+         * Решение получилось бы более громоздким по сравнению с решением через stream
+         */
+        for (Order order : orders) {
+            System.out.println(order.toString());
+        }
+
+        /*
+         * Явная логика получения следующего элемента
+         */
+        for (Iterator<Order> iterator = orders.iterator(); iterator.hasNext(); ) {
+            System.out.println(iterator.next().toString());
+        }
+
+
+        /*
+         * Очень здорово, что для таких операций сделали stream api.
+         * Для реализации группировки через цикл пришлось бы написать
+         * в несколько раз больше кода.
+         */
+        Map<OrderStatus, List<Order>> ordersByStatus = orders.stream()
+                .collect(Collectors.groupingBy(Order::getStatus));
+    }
+
+    private static void task2() {
         List<io.humb1t.Main.Order> orders = Arrays.asList(
                 new Order(OrderStatus.COMPLETED, 50),
                 new Order(OrderStatus.PROCESSING, 40),
@@ -25,17 +76,59 @@ public class Main {
         ordersTopCount.forEach(System.out::println);
     }
 
-    private void task3(){
+    private static void task3() {
+        Queue<Integer> requestQueue = new ConcurrentLinkedQueue<>();
+        Queue<Integer> responseQueue = new ConcurrentLinkedQueue<>();
 
+        ExecutorService exec = Executors.newCachedThreadPool();
+
+        // request producers
+        Stream.of(1, 2, 3, 4, 5).forEach(client -> exec.execute(() -> {
+                    IntStream.range(1, 100).parallel().forEach(requestQueue::add);
+                })
+        );
+
+        // request processors
+        Stream.of(1, 2, 3).forEach(processor -> exec.execute(() -> {
+                    while (true) {
+                        try {
+                            TimeUnit.MILLISECONDS.sleep(500L);
+                            Optional<Integer> request = Optional.ofNullable(requestQueue.poll());
+                            request.ifPresent(item -> responseQueue.offer(item * 2));
+                        } catch (InterruptedException e) {
+                            System.out.println("Thread exit");
+                            break;
+                        }
+                    }
+                })
+        );
+
+        // response printer
+        exec.execute(() -> {
+            while (true) {
+                Optional<Integer> response = Optional.ofNullable(responseQueue.poll());
+                response.ifPresent(System.out::println);
+            }
+        });
+
+        // kill 'em all
+        Timer timer = new Timer("Exit timer");
+        long delay = 30000L;
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                System.exit(0);
+            }
+        }, delay);
     }
 
-    private void task4(){
+    private static void task4() {
         Collection<String> words = Arrays.asList("hello", "hello", "world");
         Collection<String> unique = words.stream().distinct().collect(Collectors.toList());
         unique.forEach(System.out::println);
     }
 
-    private static void task5(){
+    private static void task5() {
         List<Integer> collection1 = new ArrayList<>();
         timeIt(collection1::add, 1_000_000, "ArrayList::add");
         timeIt(collection1::contains, 10_000, "ArrayList::contains");
@@ -51,11 +144,11 @@ public class Main {
         timeIt(collection2::remove, 10_000, "LinkedList::remove");
     }
 
-    private void task6(){
+    private static void task6() {
 
     }
 
-    private static void timeIt(Consumer<Integer> action, int count, String name){
+    private static void timeIt(Consumer<Integer> action, int count, String name) {
         long start = System.currentTimeMillis();
         for (int i = 0; i < count; i++) {
             action.accept(i);
