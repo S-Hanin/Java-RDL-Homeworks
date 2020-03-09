1b557019aa3f4cebfb28ad464913fbef9da97f7e
S-Hanin <sign.rx@yandex.ru>
2020-03-09 16:34:19 +0300

    functional task1

--- collections/src/person/hanin/factorial/Factorial.java
+++ collections/src/person/hanin/factorial/Factorial.java
@@ -2,21 +2,26 @@ package person.hanin.factorial;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 public class Factorial {
     private Map<Integer, Long> cache = new HashMap<>();
 
     /**
      * Calculates factorial of given number.
+     *
      * @param n int
      * @return Long
      */
     public Long calculate(int n) {
         if (n == 0) return 1L;
-        Long result = this.cache.get(n);
+        Optional<Long> cached = Optional.ofNullable(this.cache.get(n));
+        //it's pretty convenient method
+        Long result = cached.orElseGet(() -> n * calculate(n - 1));
 
-        if (result != null) return result;
-        result = n * calculate( n - 1);
+        //not far from checking for null
+//        if (cached.isPresent()) return cached.get();
+//        Long result = n * calculate( n - 1);
 
         cache.put(n, result);
         return result;
