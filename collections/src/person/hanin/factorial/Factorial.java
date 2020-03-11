2f141172072b185f5fa56782f6f4b7bf47b54d66
S-Hanin <sign.rx@yandex.ru>
2020-03-11 23:11:43 +0300

    functional: why is Optional using as parameter so bad

--- collections/src/person/hanin/factorial/Factorial.java
+++ collections/src/person/hanin/factorial/Factorial.java
@@ -4,6 +4,15 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
+
+// TODO task1: We use Optional for return values, is it a good idea
+//  to use it as incoming parameters? Write down your thoughts.
+//        1.Using Optional parameters causing conditional logic inside the methods
+//          is literally contra-productive.
+//        2.Needing to pack an argument in an Optional, is suboptimal for the compiler,
+//          and does an unnecessary wrapping.
+//        3.In comparison to nullable parameters Optional is more costly.
+
 public class Factorial {
     private Map<Integer, Long> cache = new HashMap<>();
 
@@ -18,12 +27,7 @@ public class Factorial {
         // TODO task1: Modify your Cache from previous task and return
         //  Optional#empty instead of null if element is missing
         Optional<Long> cached = Optional.ofNullable(this.cache.get(n));
-        //it's pretty convenient method
-        Long result = cached.orElseGet(() -> n * calculate(n - 1));
-
-        //not far from checking for null
-//        if (cached.isPresent()) return cached.get();
-//        Long result = n * calculate( n - 1);
+        Long result = cached.orElseGet(() -> n * calculate(n - 1)); //it's pretty convenient method
 
         cache.put(n, result);
         return result;
